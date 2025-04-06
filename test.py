1053147cae106217072e46a93609d5bced82a8f9
Chandrasekhar <64483311+chandra417@users.noreply.github.com>
2025-04-06 13:07:40 +0530

    pytest addition test

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import pytest
+
+@pytest.fixture(params=[(1,2), (2,3), (3,4), (4,5)])
+def add(request):
+    return request.param
+
+def addition(a,b):
+    return a  + b
+
+def test_addition(add):
+    assert int(add[0]) + int(add[1]) == addition(int(add[0]), int(add[1]))
